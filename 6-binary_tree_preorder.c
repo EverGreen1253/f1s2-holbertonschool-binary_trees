555882c8ff84645945e989c5274df711858f0e40
Adrian <dreamweaver3d@gmail.com>
2023-08-31 13:04:04 +0800

    trees task 6 commit 3

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -11,21 +11,19 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL || func == NULL)
+	if (tree != NULL && func != NULL)
 	{
-		exit(98);
-	}
+		func(tree->n);
 
-	func(tree->n);
+		if (tree->left != NULL)
+		{
+			binary_tree_preorder(tree->left, func);
+		}
 
-	if (tree->left != NULL)
-	{
-		binary_tree_preorder(tree->left, func);
-	}
-
-	if (tree->right != NULL)
-	{
-		binary_tree_preorder(tree->right, func);
+		if (tree->right != NULL)
+		{
+			binary_tree_preorder(tree->right, func);
+		}
 	}
 }
 
