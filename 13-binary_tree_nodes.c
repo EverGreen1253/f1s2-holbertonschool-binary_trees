a9f3e0e3ceebf4c4c115c4500efbe336c2d6f90e
Adrian <dreamweaver3d@gmail.com>
2023-08-31 16:58:15 +0800

    trees task 13 commit 1

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - count nodes with at least 1 child
+ * @tree: the tree
+ *
+ * Return: the node
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t value = 1;
+
+	if ((tree == NULL) || (tree->left == NULL && tree->right == NULL))
+	{
+		return (0);
+	}
+
+	if (tree->left != NULL)
+	{
+		value = value + binary_tree_nodes(tree->left);
+	}
+
+	if (tree->right != NULL)
+	{
+		value = value + binary_tree_nodes(tree->right);
+	}
+
+	return (value);
+}
+
