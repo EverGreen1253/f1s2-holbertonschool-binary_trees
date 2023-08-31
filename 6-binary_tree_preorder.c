e0ce24d886add5e1a8a51cfcb0b39e018d103f29
Adrian <dreamweaver3d@gmail.com>
2023-08-31 12:30:24 +0800

    trees task 6 commit 1

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - traversal using preorder
+ * @tree: the tree
+ * @func: function pointer
+ *
+ * Return: the node
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL)
+	{
+		exit(98);
+	}
+
+	func(tree->n);
+
+	if (tree->left != NULL)
+	{
+		binary_tree_preorder(tree->left, func);
+	}
+
+	if (tree->right != NULL)
+	{
+		binary_tree_preorder(tree->right, func);
+	}
+}
+
