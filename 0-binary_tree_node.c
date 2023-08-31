383bb7a6de70184b50cc6cc2e7b15a083e8b8a37
Adrian <dreamweaver3d@gmail.com>
2023-08-31 10:00:21 +0800

    trees task 0 commit 1

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates and populates a node
+ * @parent: the parent node
+ * @value: the value to chuck in
+ *
+ * Return: the node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *b;
+
+	b = malloc(sizeof(binary_tree_t));
+	if (b == NULL)
+	{
+		return (NULL);
+	}
+	
+	b->parent = parent;
+	b->n = value;
+	b->left = NULL;
+	b->right = NULL;
+
+	return (b);
+}
