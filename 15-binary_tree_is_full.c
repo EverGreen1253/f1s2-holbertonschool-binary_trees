731164948e2039d1eb074c6d416a322fd1269b68
Adrian <dreamweaver3d@gmail.com>
2023-09-02 22:13:16 +0800

    Trees task 15 commit 1

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - check if tree full
+ * @tree: the tree
+ *
+ * Return: 0 or 1
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int is_full = 1;
+
+	if (tree->left == NULL && tree->right == NULL)/* leaf */
+	{
+		return (1);
+	}
+
+	if (tree == NULL || tree->left == NULL || tree->right == NULL)/* not leaf */
+	{
+		return (0);
+	}
+
+	is_full = binary_tree_is_full(tree->left);
+	if (is_full == 0)
+	{
+		/* no point in checking the other branch */
+		return (0);
+	}
+
+	is_full = is_full && binary_tree_is_full(tree->right);
+
+	return (is_full);
+}
+
